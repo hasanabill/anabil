2c1a3bf224451144ee6523498e828dd365400c81
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2025-02-04 23:25:25 +0600

    Initialized and Navbar made

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <main className="max-w-7xl mx-auto">
+        <Navbar />
+      </main>
+    </>
+  );
+};
+
+export default App;
