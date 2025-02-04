b5d89871a144f177bce13d6c2febd28903d70011
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2025-02-04 23:35:50 +0600

    starting 3d

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3"></div>
+    </section>
+  );
+};
+
+export default Hero;
