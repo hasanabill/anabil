e42e924555777082a94bb5bc8602088a6e6b27ad
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2025-02-26 01:17:28 +0600

    nan

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,16 @@
 const Hero = () => {
   return (
     <section className="min-h-screen w-full flex flex-col relative">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3"></div>
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi, I am Anabil <span className="waving-hand">✋🏽</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">threeeeeeeeeee js</div>
     </section>
   );
 };
